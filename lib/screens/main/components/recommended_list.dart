130282f9a36b6b9dbae528f18427fb0d7b7d99e8
wanfer3434 <wanfer2121@gmail.com>
2024-06-14 22:43:53 -0500

    cambios en recommended_list

--- lib/screens/main/components/recommended_list.dart
+++ lib/screens/main/components/recommended_list.dart
@@ -9,8 +9,7 @@ class RecommendedList extends StatelessWidget {
     Product('assets/G54.jpg', 'Bag', 'Beautiful bag', 2.33),
     Product('assets/funda-silicona-suave-con-cubreobjetivo-para-oppo-a-79-5g.png', 'Cap', 'Cap with beautiful design', 10),
     Product('assets/funda-silicona-suave-samsung-a14-5g-con-camara-3d-7-colores.jpg', 'Jeans', 'Jeans for you', 20),
-    Product('assets/Iphon13_Corazzones.jpg', 'Woman Shoes',
-        'Shoes with special discount', 30),
+    Product('assets/Iphon13_Corazzones.jpg', 'Woman Shoes', 'Shoes with special discount', 30),
     Product('assets/Redmi_13c_5g.png', 'Bag Express', 'Bag for your shops', 40),
     Product('assets/Redmi_Note_10s.jpg', 'Jeans', 'Beautiful Jeans', 102.33),
     Product('assets/Redmi_note_12.png', 'Silver Ring', 'Description', 52.33),
@@ -23,7 +22,7 @@ class RecommendedList extends StatelessWidget {
     return Column(
       children: <Widget>[
         SizedBox(
-          height: 20,
+          height: 40,
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
@@ -35,48 +34,57 @@ class RecommendedList extends StatelessWidget {
                 ),
               ),
               Center(
-                  child: Text(
-                'Recommended',
-                style: TextStyle(
+                child: Text(
+                  'Recommended',
+                  style: TextStyle(
                     color: darkGrey,
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold),
-              )),
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
             ],
           ),
         ),
         Flexible(
           child: Container(
-            padding: EdgeInsets.only(top: 16.0, right: 16.0, left: 16.0),
+            padding: EdgeInsets.all(16.0),
             child: MasonryGridView.count(
               physics: NeverScrollableScrollPhysics(),
               padding: EdgeInsets.zero,
-              crossAxisCount: 4,
+              crossAxisCount: 2,  // Total de columnas en la cuadrícula
               itemCount: products.length,
-              itemBuilder: (BuildContext context, int index) => new ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              itemBuilder: (BuildContext context, int index) => ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
                 child: InkWell(
                   onTap: () => Navigator.of(context).push(MaterialPageRoute(
-                      builder: (_) => ProductPage(product: products[index]))),
+                    builder: (_) => ProductPage(product: products[index]),
+                  )),
                   child: Container(
-                      decoration: BoxDecoration(
-                        gradient: RadialGradient(
-                            colors: [
-                              Colors.grey.withOpacity(0.3),
-                              Colors.grey.withOpacity(0.7),
-                            ],
-                            center: Alignment(0, 0),
-                            radius: 0.6,
-                            focal: Alignment(0, 0),
-                            focalRadius: 0.1),
+                    decoration: BoxDecoration(
+                      gradient: RadialGradient(
+                        colors: [
+                          Colors.grey.withOpacity(0.3),
+                          Colors.grey.withOpacity(0.7),
+                        ],
+                        center: Alignment(0, 0),
+                        radius: 0.6,
+                        focal: Alignment(0, 0),
+                        focalRadius: 0.1,
                       ),
-                      child: Hero(
-                          tag: products[index].image,
-                          child: Image.asset(products[index].image))),
+                    ),
+                    child: Hero(
+                      tag: products[index].image,
+                      child: Image.asset(
+                        products[index].image,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
                 ),
               ),
-              mainAxisSpacing: 4.0,
-              crossAxisSpacing: 4.0,
+              mainAxisSpacing: 16.0,
+              crossAxisSpacing: 16.0,
             ),
           ),
         ),
