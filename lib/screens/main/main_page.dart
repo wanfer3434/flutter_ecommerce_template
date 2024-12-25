5ac9628f9e402416023f223a5514a2342064ea03
wanfer3434 <wanfer2121@gmail.com>
2024-12-25 02:15:01 -0500

    cambios en e main_page25

--- lib/screens/main/main_page.dart
+++ lib/screens/main/main_page.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:ecommerce_int2/models/product.dart';
 import 'package:ecommerce_int2/screens/notifications_page.dart';
 import 'package:ecommerce_int2/screens/profile_page.dart';
 import 'package:ecommerce_int2/screens/shop/check_out_page.dart';
+import '../../custom_background.dart';
 import '../../models/local_product_list.dart';
 import '../category/category_list_page.dart';
 import '../chat_page.dart';
@@ -13,6 +15,7 @@ import 'components/custom_bottom_bar.dart';
 import 'components/product_list.dart';
 import 'components/tab_view.dart';
 
+
 class MainPage extends StatefulWidget {
   @override
   _MainPageState createState() => _MainPageState();
@@ -20,22 +23,73 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
   late TabController tabController;
-  List<Product> products = [];
-  TextEditingController chatController = TextEditingController();
+  late TabController bottomTabController;
+  List<String> timelines = ["Hoy", "Semana", "Mes"];
+  String selectedTimeline = "Hoy";
   List<Map<String, String>> chatMessages = [];
+  TextEditingController chatController = TextEditingController();
+
+  final List<Product> products = [
+    // Llena esta lista con instancias de tu clase Product
+    Product(
+      id: '1',
+      name: 'Producto 1',
+      description: 'Descripción del producto 1',
+      imageUrls: ['https://via.placeholder.com/150'],
+      price: 29.99,
+      averageRating: 4.5,
+      ratingCount: 100,
+    ),
+    Product(
+      id: '2',
+      name: 'Producto 2',
+      description: 'Descripción del producto 2',
+      imageUrls: ['https://via.placeholder.com/150'],
+      price: 49.99,
+      averageRating: 4.7,
+      ratingCount: 200,
+    ),
+  ];
 
   @override
   void initState() {
     super.initState();
     tabController = TabController(length: 5, vsync: this);
-    products = LocalProductService().getProducts();
+    bottomTabController = TabController(length: 4, vsync: this);
   }
 
-  @override
-  void dispose() {
-    tabController.dispose();
-    chatController.dispose();
-    super.dispose();
+  Widget _buildProductList() {
+    return ProductList(
+      products: products,
+    );
+  }
+
+  Future<void> sendMessage(String userMessage) async {
+    if (userMessage.isEmpty) return;
+
+    setState(() {
+      chatMessages.add({'user': userMessage, 'bot': 'Cargando...'});
+    });
+
+    try {
+      final responseDoc = await FirebaseFirestore.instance
+          .collection('responses')
+          .where('input', isEqualTo: userMessage.toLowerCase())
+          .limit(1)
+          .get();
+
+      String botResponse = responseDoc.docs.isNotEmpty
+          ? responseDoc.docs.first['response']
+          : 'Lo siento, no entiendo tu mensaje. ¿Podrías reformularlo?';
+
+      setState(() {
+        chatMessages.last['bot'] = botResponse;
+      });
+    } catch (e) {
+      setState(() {
+        chatMessages.last['bot'] = 'Hubo un error, intenta más tarde.';
+      });
+    }
   }
 
   Widget _buildChatInterface() {
@@ -81,54 +135,43 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
     );
   }
 
-  Future<void> sendMessage(String userMessage) async {
-    if (userMessage.isEmpty) return;
-
-    setState(() {
-      chatMessages.add({'user': userMessage, 'bot': 'Cargando...'});
-    });
-
-    try {
-      final responseDoc = await FirebaseFirestore.instance
-          .collection('responses')
-          .where('input', isEqualTo: userMessage.toLowerCase())
-          .limit(1)
-          .get();
-
-      String botResponse = responseDoc.docs.isNotEmpty
-          ? responseDoc.docs.first['response']
-          : 'Lo siento, no entiendo tu mensaje. ¿Podrías reformularlo?';
-
-      setState(() {
-        chatMessages.last['bot'] = botResponse;
-      });
-    } catch (e) {
-      setState(() {
-        chatMessages.last['bot'] = 'Hubo un error, intenta más tarde.';
-      });
-    }
-  }
-
-  Widget _buildProductList() {
-    return ProductList(
-      products: products,
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    final tabBar = TabBar(
+    Widget topHeader = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        for (var timeline in timelines)
+          Flexible(
+            child: InkWell(
+              onTap: () {
+                setState(() {
+                  selectedTimeline = timeline;
+                });
+              },
+              child: Text(
+                timeline,
+                style: TextStyle(
+                  fontSize: timeline == selectedTimeline ? 20 : 14,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ),
+      ],
+    );
+
+    Widget tabBar = TabBar(
       tabs: [
-        Tab(text: 'Explorar'),
         Tab(text: 'Tendencia'),
         Tab(text: 'Deportes'),
         Tab(text: 'Audífonos'),
         Tab(text: 'Inalámbricos'),
+        Tab(text: 'Bluetooth'),
       ],
       labelStyle: TextStyle(fontSize: 16.0),
       unselectedLabelStyle: TextStyle(fontSize: 14.0),
-      labelColor: Colors.black,
-      unselectedLabelColor: Colors.grey,
+      labelColor: Colors.grey,
+      unselectedLabelColor: Color.fromRGBO(0, 0, 0, 0.5),
       isScrollable: true,
       controller: tabController,
     );
@@ -136,75 +179,33 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
     return Scaffold(
       appBar: AppBar(
         title: Text('Tu Tienda'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.notifications),
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(builder: (_) => NotificationsPage()),
-              );
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.chat),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ChatScreen()),
-              );
-            },
-          ),
-        ],
-        bottom: PreferredSize(
-          preferredSize: tabBar.preferredSize,
-          child: tabBar,
-        ),
       ),
-      bottomNavigationBar: CustomBottomBar(controller: tabController),
-      body: TabBarView(
-        controller: tabController,
-        children: <Widget>[
-          // Primera pestaña: Explorar
-          SafeArea(
-            child: CustomScrollView(
-              slivers: <Widget>[
-                SliverAppBar(
-                  expandedHeight: 250,
-                  pinned: true,
-                  flexibleSpace: FlexibleSpaceBar(
-                    background: BannerWidget(
-                      imageUrl: 'https://i.imgur.com/GaEsmRG.png',
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => AnotherPage()),
-                        );
-                      },
-                    ),
-                  ),
-                ),
-                SliverList(
-                  delegate: SliverChildListDelegate([
-                    TabView(tabController: tabController),
-                  ]),
-                ),
-              ],
+      body: CustomPaint(
+        painter: MainBackground(),
+        child: TabBarView(
+          controller: bottomTabController,
+          physics: NeverScrollableScrollPhysics(),
+          children: <Widget>[
+            SafeArea(
+              child: NestedScrollView(
+                headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+                  return <Widget>[
+                    SliverToBoxAdapter(child: topHeader),
+                    SliverToBoxAdapter(child: _buildProductList()),
+                    SliverToBoxAdapter(child: tabBar),
+                  ];
+                },
+                body: TabView(tabController: tabController),
+              ),
             ),
-          ),
-
-          // Segunda pestaña: Tendencia
-          _buildProductList(),
-
-          // Tercera pestaña: Categorías
-          CategoryListPage(),
-
-          // Cuarta pestaña: Checkout
-          CheckOutPage(),
-
-          // Quinta pestaña: Perfil
-          ProfilePage(),
-        ],
+            CategoryListPage(),
+            CheckOutPage(),
+            _buildChatInterface(),
+          ],
+        ),
       ),
     );
   }
 }
+
+
